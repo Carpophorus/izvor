51296f59f2267da987155e51e9bddd12cb0b1c9e
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-04-02 20:55:44 +0200

    updated mail script

--- mail-script.php
+++ mail-script.php
@@ -5,17 +5,17 @@
     $email_subject = "Pekara Izvor - Poruka iz kontakt-forme";
 
     function died($error) {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        echo "Postoje greške u Vašoj formi. ";
+        echo "Greške su navedene ispod.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "Molimo Vas, vratite se na formu i ispravite navedene greške.<br /><br />";
         die();
     }
 
     if(!isset($_POST['name']) ||
         !isset($_POST['e-mail']) ||
         !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
+        died('Neko polje je nepopunjeno.');
     }
 
     $name = $_POST['name']; // required
@@ -26,16 +26,16 @@
  
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
     if(!preg_match($email_exp,$email_from)) {
-      $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+      $error_message .= 'Uneta e-mail adresa nije u validnom formatu.<br />';
     }
    
     $string_exp = "/^[A-Za-z .'-ŠĐČĆŽšđčćž]+$/";
     if(!preg_match($string_exp,$name)) {
-      $error_message .= 'The Name you entered does not appear to be valid.<br />';
+      $error_message .= 'Ime koje ste uneli nije u validnom formatu.<br />';
     }
 
     if(strlen($comments) < 2) {
-      $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+      $error_message .= 'Poruka koju ste uneli nije u validnom formatu.<br />';
     }
    
     if(strlen($error_message) > 0) {
@@ -59,7 +59,7 @@
     @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
 
-  Thank you for contacting us. We will be in touch with you very soon.
+  Hvala Vam na Vašim komentarima i ukazanom poverenju!
 
 <?php
 }
